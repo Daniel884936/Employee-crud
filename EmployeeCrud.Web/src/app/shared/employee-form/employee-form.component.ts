4925361b35ecfd59370d8a3b890544f788de84e6
Daniel <tejadadaniel33@gmail.com>
2021-04-20 21:57:38 -0400

    form validations and added a toast

--- EmployeeCrud.Web/src/app/shared/employee-form/employee-form.component.ts
+++ EmployeeCrud.Web/src/app/shared/employee-form/employee-form.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from 'src/app/models/employee.model';
 import { EmployeeService } from 'src/app/services/employee.service';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import Swal from 'sweetalert2';
+
+
 
 @Component({
   selector: 'app-employee-form',
@@ -34,6 +37,37 @@ export class EmployeeFormComponent implements OnInit {
     }
   }
 
+  isInValidField(controlName:string):boolean{
+    const control =  this.formGroup.get(controlName);
+    return !control.valid && control.touched;
+  }
+
+   hasRequiredError(controlName:string):boolean {
+    const control =  this.formGroup.get(controlName);
+    return control.errors?.required && control.touched;
+  }
+
+  hasMaxLengthError(controlName:string):boolean{
+    const control =  this.formGroup.get(controlName);
+    return control.errors?.maxlength && control.touched;
+  }
+
+  hasMinError(controlName:string):boolean{
+    const control =  this.formGroup.get(controlName);
+    return control.errors?.min && control.touched;
+  }
+
+  hasMaxError(controlName:string):boolean{
+    const control =  this.formGroup.get(controlName);
+    return control.errors?.max && control.touched;
+  }
+
+   get hasEmailFormatError():boolean {
+    const control =  this.formGroup.get('email');
+    return control.errors?.email && control.touched;
+  }
+
+
   public onBackTolist():void{
     if(this.employeId){
       this._router.navigate(['../../list']);
@@ -44,15 +78,20 @@ export class EmployeeFormComponent implements OnInit {
 
   private initForm():void{
     this.formGroup = this._formBuilder.group({
-      email : [''],
-      name : [''],
-      surnames : [''],
-      age : [''],
-      phoneNumber : ['']
+      email : ['', [Validators.required, Validators.maxLength(30), Validators.email]],
+      name : ['',[Validators.required, Validators.maxLength(30)]],
+      surnames : ['',[Validators.required, Validators.maxLength(30)]],
+      age : ['',[Validators.required, Validators.max(2147483647), Validators.min(-2147483647)]],
+      phoneNumber : ['',[Validators.required,Validators.max(2147483647),Validators.min(-2147483647)]]
     });
   }
 
   public  onSubmit():void{
+    console.log(this.formGroup)
+    if(!this.formGroup.valid){
+      this.maskInvalidControlsAsTouched();
+      return;
+    }
     const employee = this.formGroup.value;
     if(this.employeId){
       this.updateEmployee(employee);
@@ -62,9 +101,17 @@ export class EmployeeFormComponent implements OnInit {
     this.addEmployee(employee);
   }
 
+  private maskInvalidControlsAsTouched():void{
+    Object.values(this.formGroup.controls).forEach(control=>{
+      if(control.invalid)
+        control.markAsTouched();
+    })
+  }
+
   private updateEmployee(employee:Employee):void{
     this._employeeService.update(this.employeId,employee).subscribe(data=>{
       console.log(data);
+      this.disPlayToast('Updated successfully');
     },(err)=>{
       console.log(err);
     });
@@ -73,19 +120,41 @@ export class EmployeeFormComponent implements OnInit {
   private addEmployee(employee:Employee):void{
     this._employeeService.add(employee).subscribe(data=>{
       console.log(data);
+      this.formGroup.reset();
+      this.disPlayToast('Saved successfully');
     },(err:any)=>{
       console.log(err);
     });
   }
 
-  private  employeeById(employeeId:number){
+  private employeeById(employeeId:number){
        this._employeeService.getById(employeeId).subscribe(employeeApi=>{
          if(employeeApi){
            this.formGroup.patchValue(employeeApi);
            return;
          }
          this._router.navigate(['../list']);
-      })
+      });
   }
 
+  private disPlayToast(title:string):void{
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'bottom-left',
+      showConfirmButton: false,
+      timer: 2500,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    Toast.fire({
+      icon: 'success',
+      title: title
+    })
+  }
+
+
 }
+
